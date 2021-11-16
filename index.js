29754bc2b9f65d7ae520f1dff8aabe323c81eec3
kiescha <90151742+kiescha@users.noreply.github.com>
2021-11-16 10:57:17 +0100

    Complete mess

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
  
-const express = require('express'),    
-    morgan = require('morgan');
+const express = require('express');  
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
 
 // API
 let topMovies =[
     {
         title: 'Warcraft: The Beginning',
-        author: 'Charles Leavitt and Duncan Jones'
+        author: {
+            name: 'Charles Leavitt and Duncan Jones',
+            birth: '1234',
+            death: 'not yet'
+        },
+        genre: 'Sci-Fi'
     },
     {
         title: 'The Shawshank Redemption',
-        author: 'Stephen King'
+        author: {
+            name: 'Stephen King',
+            birth: '1947',
+            death: 'not yet'
+        },           
+        genre: 'Drama'
     },
     {
         title: 'The Magic of Belle Isle',
-        author: 'Guy Thomas, Rob Reiner, Andrew Scheinman'
+        author: {
+            name: 'Guy Thomas, Rob Reiner, Andrew Scheinman',     
+            birth: '1999',
+            death: 'not yet'
+        },
+        genre: ['Comedy', 'Drama']
     },
     {
         title: 'Dota: Dragon\'s Blood',
-        author: 'Ashley Edward Miller'
+        author: {
+            name: 'Ashley Edward Miller',        
+            birth: '2000',
+            death: 'not yet'
+        },
+        genre: ['Action', 'Sci-Fi', 'Fantasy']
     },
     {
         title: 'The Witcher',
-        author: 'Andrzej Sapkowski'
+        author: {
+            name: 'Andrzej Sapkowski',
+            birth: '2222',
+            death: 'not yet'
+        },
+        genre: ['Drama', 'Fantasy', 'Adventure']
     },
 ];
 //Get Request
@@ -33,9 +61,33 @@ app.get('/', (req, res) => {
 app.get('/documentation', (req, res) => {
     res.sendfile('public/documentation.html', {root: __dirname})
 }),
+
+// Will GET list of movies.
 app.get('/movies', (req, res) => {
     res.json(topMovies);
 }),
+
+// Gets the data about a specific movie.
+app.get('/movies/:title', (req, res) => {
+    res.json(topMovies.find((movie) => {
+        return movie.title === req.params.title
+    }));
+});
+
+
+// Return data about a genre by name/title.
+app.get('/genres/:Name', (req, res) => {
+    res.json(topMovies.find((genres)=> {
+        return genres.genre === req.params.genre
+    }));
+})
+
+// Get info about author
+// app.get('/movies/author/:name', (req, res) => {
+//     res.json(topMovies.author.find((author) => {
+//         return author.name === req.params.name
+//     }))
+// })
 
 //Morgan Logger
 app.use(morgan('common'));
